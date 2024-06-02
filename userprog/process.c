7c0312d79a3da9397084ed4e593f5338e0467cee
bada308 <203190@jnu.ac.kr>
2024-06-02 10:34:10 +0000

    feat: lazy_load_segment를 위한 보조 값 전달

--- userprog/process.c
+++ userprog/process.c
@@ -812,18 +812,27 @@ load_segment(struct file *file, off_t ofs, uint8_t *upage,
 		size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
 		size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
-		/* TODO: [VM] `aux` 인수로 제공할 보조 값 설정하기 */
-		/* TODO: [VM] 바이너리를 로드하는 데 필요한 정보를 포함하는 구조체를 생성하는 것이 좋다. */
-		/* TODO: Set up aux to pass information to the lazy_load_segment. */
-		void *aux = NULL;
+		/* NOTE: [VM] `aux` 인수로 제공할 보조 값 설정하기 */
+		/* NOTE: [VM] 바이너리를 로드하는 데 필요한 정보를 포함하는 구조체를 생성하는 것이 좋다. */
+		/* NOTE: Set up aux to pass information to the lazy_load_segment. */
+		struct page_load_info *page_load_info = malloc(sizeof(struct page_load_info));
+		page_load_info->file = file;
+		page_load_info->offset = ofs;
+		page_load_info->read_bytes = page_read_bytes;
+		page_load_info->zero_bytes = page_zero_bytes;
+
 		if (!vm_alloc_page_with_initializer(VM_ANON, upage,
-											writable, lazy_load_segment, aux))
+											writable, lazy_load_segment, (void *)page_load_info))
+		{
+			free(page_load_info);
 			return false;
+		}
 
 		/* Advance. */
 		read_bytes -= page_read_bytes;
 		zero_bytes -= page_zero_bytes;
 		upage += PGSIZE;
+		ofs += page_read_bytes; /* offset 업데이트 */
 	}
 	return true;
 }
