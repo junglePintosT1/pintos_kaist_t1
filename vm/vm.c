327a63f703a417a5fbb8e445d7ed60a5510a6c31
bada308 <203190@jnu.ac.kr>
2024-06-03 05:46:33 +0000

    feat: vm_try_handle_fault에 예외처리 추가 - 주어진 주소가 NULL일 경우 - 커널 가상 주소가 전달된 경우 - not_present가 false인 경우 (read-only 파일에 접근) - 쓰기 불가능한 파일에 쓰기 작업을 하려는 경우

--- vm/vm.c
+++ vm/vm.c
@@ -202,16 +202,25 @@ vm_handle_wp(struct page *page UNUSED)
 bool vm_try_handle_fault(struct intr_frame *f UNUSED, void *addr UNUSED,
 						 bool user UNUSED, bool write UNUSED, bool not_present UNUSED)
 {
-	bool success = false;
 	struct supplemental_page_table *spt UNUSED = &thread_current()->spt;
 	struct page *page = NULL;
-	/* TODO: Validate the fault */
-	/* TODO: Your code goes here */
-	page = spt_find_page(spt, addr);
-	if (page != NULL)
-		success = vm_do_claim_page(page);
 
-	return success;
+	if (addr == NULL)
+		return false;
+
+	if (is_kernel_vaddr(addr))
+		return false;
+
+	if (not_present)
+	{
+	page = spt_find_page(spt, addr);
+		if (page == NULL)
+			return false;
+		if (write == 1 && page->writable == 0)
+			return false;
+		return vm_do_claim_page(page);
+	}
+	return false;
 }
 
 /* Free the page.
